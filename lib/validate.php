e67e88ff4457bcbe8390f26261d1885376804f3b
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-03-18 10:14:59 -0500

    fix:se elimina metodo de curl para obtener los datos y se cambia endpoint de consulta

--- lib/validate.php
+++ lib/validate.php
@@ -1,50 +1,29 @@
 <?php
 if (is_array($_REQUEST) && count($_REQUEST) > 0) {
-
+    $username=$_REQUEST['epayco_publickey'];
+    $password=$_REQUEST['epayco_privatey'];
     $data = array(
-        'public_key' => $_REQUEST['epayco_publickey'],
-        'private_key' => $_REQUEST['epayco_privatey']
+        'public_key' => $username,
+        'private_key' => $password
     );
-    if (function_exists('curl_init')) {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://api.secure.payco.co/v1/auth/login',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>json_encode($data),
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-        ));
-
-        $response = curl_exec($curl);
-        if ($response === false) {
-            return array('curl_error' => curl_error($curl), 'curerrno' => curl_errno($curl));
-        }
-        curl_close($curl);
-    }else{
-
-        $content = json_encode($data);
-		$header = array(
-			"Content-Type: application/json"
-		);
-		$options = array(
-			'http' => array(
-				'method' => 'POST',
-				'content' => $content,
-				'header' => implode("\r\n", $header)
-			)
-			
-		);
-        $response =file_get_contents('https://api.secure.payco.co/v1/auth/login', false, stream_context_create($options));
-    }
+    
+    $content = json_encode($data);
+    $header = array(
+        "Authorization: Basic " . base64_encode("$username:$password")."\r\n".
+        "Content-Type: application/json"
+    );
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'content' => $content,
+            'header' => "Authorization: Basic " . base64_encode("$username:$password")."\r\n".
+            "Content-Type: application/json\r\n"
+        )
+        
+    );    
+    $response =file_get_contents('https://api.secure.payco.co/v1/auth/login', false, stream_context_create($options));
     $data = json_decode($response);
-    $status =  $data->status;
+    $status =  $data;
     echo $status;
 }else{
     echo 0;
