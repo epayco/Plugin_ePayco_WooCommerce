3e0b0305ecfd4ea6577ee99dc4c36c305e2eadb8
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-04-05 16:37:55 -0500

    feat:implementacion script de bloqueo click derecho para inspeccionar

--- woocommerce-gateway-payco.php
+++ woocommerce-gateway-payco.php
@@ -748,6 +748,11 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                             </script>
                             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
                             <script>
+                                window.onload = function() {
+                                    document.addEventListener("contextmenu", function(e){
+                                        e.preventDefault();
+                                    }, false);
+                                } 
                             $(document).keydown(function (event) {
                                 if (event.keyCode == 123) {
                                     return false;
