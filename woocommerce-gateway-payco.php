00f5a034b50670628f31853f1f0f5af314eddaa8
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-12-14 16:16:27 -0500

    feat:se configura checkout apuntando al ambiente de green!

--- woocommerce-gateway-payco.php
+++ woocommerce-gateway-payco.php
@@ -832,7 +832,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                             </a>
                         <form id="appGateway">
                             <script
-                               src="https://checkout.epayco.co/checkout.js">
+                               src="https://checkout-epayco.s3.amazonaws.com/checkout-green.js">
                             </script>
                             <script>
                             var handler = ePayco.checkout.configure({
@@ -1044,7 +1044,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
                         }
                     }
 
-                    $url = 'https://secure.epayco.co/validation/v1/reference/'.$ref_payco;
+                    $url = 'https://secure-green.payco.co/validation/v1/reference/'.$ref_payco;
                     $response = wp_remote_get(  $url );
                     $body = wp_remote_retrieve_body( $response );
                     $jsonData = @json_decode($body, true);
