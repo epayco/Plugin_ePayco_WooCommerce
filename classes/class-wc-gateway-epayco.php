94806cf5ac437c4a2c933a3c2ba3807b8f4b8322
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2024-04-05 09:52:12 -0500

    feat:se configura checkout de pre-prod

--- classes/class-wc-gateway-epayco.php
+++ classes/class-wc-gateway-epayco.php
@@ -489,7 +489,7 @@ class WC_Gateway_Epayco extends WC_Payment_Gateway {
                         headers["privatekey"] = privatekey;
                         headers["apikey"] = apikey;
                         var payment =   function (){
-                            return  fetch("https://cms.epayco.co/checkout/payment/session", {
+                            return  fetch("https://cms.epayco.io/checkout/payment/session", {
                                 method: "POST",
                                 body: JSON.stringify(info),
                                 headers
