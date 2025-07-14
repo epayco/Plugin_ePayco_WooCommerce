92b7132d915f63ef9708c4735155723e0d0307fe
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-07-14 11:18:00 -0500

    fix:Se ajusta valores de impuestos para pagos con descuentos

--- classes/class-wc-gateway-epayco.php
+++ classes/class-wc-gateway-epayco.php
@@ -313,7 +313,7 @@ class WC_Gateway_Epayco extends WC_Payment_Gateway
         //$iva = $iva !== 0 ? $iva :$order->get_total_tax();
 
         //$base_tax = ($iva !== 0) ? ($order->get_total() - $order->get_total_tax()): (($ico !== 0) ? ($order->get_total() - $order->get_total_tax()): $order->get_subtotal() );
-        $base_tax = $order->get_total() - $iva;
+        $base_tax = $order->get_total() - $iva - $ico;
             
         foreach ($order->get_items() as $product) {
             $clearData = str_replace('_', ' ', $this->string_sanitize($product['name']));
